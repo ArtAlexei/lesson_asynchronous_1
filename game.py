a6fb683ede9404be4702f0dac0bacfbc4dcfcab7
alexei <ArtAlexeiMail@gmail.com>
2022-07-29 18:04:51 +0300

    add fire

--- game.py
+++ game.py
@@ -1,19 +1,89 @@
 import time
+import asyncio
 import curses
+import random
+
+TIC_TIMEOUT=0.1
 
 
-def draw(canvas):
-    row, column = (5, 20)
+async def fire(canvas, start_row, start_column, rows_speed=-0.3, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
+async def blink(canvas, row, column, offset_tics=0, symbol='*'):
+    while True:
+
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        for _ in range(20):
+            await asyncio.sleep(0)
+        
+        for _ in range(offset_tics):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        for _ in range(5):
+            await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        for _ in range(3):
+            await asyncio.sleep(0)
+
+
+def draw(canvas,page_rows,page_columns):
     canvas.border()
-    canvas.addstr(row, column, 'Hello, World!')
-    canvas.refresh()
-    canvas.refresh()
-
-    time.sleep(3)
+    coroutines=[fire(canvas,page_rows//2,page_columns//2)]
+    for index in range(100):
+        row=random.randint(1,page_rows-2)
+        column=random.randint(1,page_columns-2)
+        offset_tics = random.randint(1,30)
+        symbol=random.choice('+*.:')
+        coroutines.append(blink(canvas,row ,column,offset_tics,symbol))
+    
+    while True:
+        for coroutine in coroutines.copy():
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.remove(coroutine)
+        time.sleep(TIC_TIMEOUT)
+        canvas.refresh()
+   
 
 
 if __name__ == '__main__':
-    curses.initscr()
+    window = curses.initscr()
     curses.curs_set(False)
     curses.update_lines_cols()
-    curses.wrapper(draw)
+    page_rows,page_columns =window.getmaxyx()
+    curses.wrapper(draw,page_rows,page_columns)
+
